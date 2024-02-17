429851a479b412235665f946809888b87a1a7472
Siddharth Singh <siddharth@xola.com>
2024-02-17 18:36:11 +0530

    Work test

--- src/App.js
+++ src/App.js
@@ -5,17 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, I'm Siddarth. Work in progress.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://instagram.com/cddharthsingh"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Meet me here instead
         </a>
       </header>
     </div>
