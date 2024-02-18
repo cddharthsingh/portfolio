5ac4da27072a5ca61f79eda2d6c074e305c63754
Siddharth Singh <siddharth@xola.com>
2024-02-18 18:22:15 +0530

    Setting up the portfolio

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 // src/App.js
 import React from 'react';
-import Layout from './components/Layout';
-import DynamicGradient from './components/DynamicGradient';
+import Layout from "./components/Layout";
 import './index.css'; // Ensure this imports Tailwind CSS
 
 function App() {
   return (
-    <div className="relative">
-      <DynamicGradient />
-      <Layout>
-        <h1 className="text-6xl font-bold text-white">Siddharth Singh</h1>
-      </Layout>
-    </div>
+    <Layout>
+    </Layout>
   );
 }
 
