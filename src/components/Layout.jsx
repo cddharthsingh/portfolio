e9508a45baa018a21f5115d5a8134492d46e4cd6
Siddharth Singh <siddharth@xola.com>
2024-02-18 19:22:52 +0530

    Creating vs code like structure

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,16 @@
 // src/components/Layout.js\
-// import Sidebar from "./Sidebar";
+import Sidebar from "./Sidebar";
+import BottomBlock from "./BottomBlock";
 
 export default function Layout() {
   return (
     <div className="flex h-screen bg-zinc-900">
-      {/* <Sidebar></Sidebar> */}
-      <div className="flex items-center justify-center w-5/6">
-        <p class="text-9xl poppins-black text-cyan-500"> Siddharth Singh</p>
+      <Sidebar></Sidebar>
+      <div className="w-5/6">
+        <div className="flex items-center justify-center h-4/5 p-5">
+          <p class="lg:text-9xl xs:text-5xl sm:text-5xl poppins-bold text-cyan-500"> Siddharth Singh</p>
+        </div>
+        <BottomBlock></BottomBlock>
       </div>
     </div>
   );
