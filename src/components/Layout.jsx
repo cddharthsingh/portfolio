116a9b1f361f034048fc41ade8117e25be9ae556
Siddharth Singh <siddharth@xola.com>
2024-02-20 16:32:46 +0530

    implemented adjustable sidebar width

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,30 @@
-// src/components/Layout.js\
-import Sidebar from "./Sidebar";
-import BottomBlock from "./BottomBlock";
+import React, { useState, useRef } from 'react';
+import Sidebar from './Sidebar';
+import BottomBlock from './BottomBlock';
 import LineColumn from './LineColumn';
 import MainArea from './MainArea';
 
 export default function Layout() {
+    const [sidebarWidth, setSidebarWidth] = useState(200);
+    const dragBarWidth = useRef(null);
+
+    const startResizing = (e) => {
+        document.addEventListener('mousemove', resize);
+        document.addEventListener('mouseup', stopResizing);
+    };
+
+    const resize = (e) => {
+        setSidebarWidth(e.clientX);
+    };
+
+    const stopResizing = () => {
+        document.removeEventListener('mousemove', resize);
+        document.removeEventListener('mouseup', stopResizing);
+    };
     return (
         <div className="flex h-screen bg-zinc-900">
-            <Sidebar></Sidebar>
+            <Sidebar width={sidebarWidth}></Sidebar>
+            <div ref={dragBarWidth} onMouseDown={startResizing} className="cursor-col-resize bg-zinc-950 w-1" />
             <div className="flex flex-col w-5/6 relative">
                 <div className="flex overflow-auto h-4/5 hide-scrollbar">
                     <LineColumn></LineColumn>
--- src/components/MainArea.jsx
+++ src/components/MainArea.jsx
@@ -1,7 +1,7 @@
 export default function MainArea() {
     return (
         <div className="flex flex-1 flex-col items-center justify-center p-5">
-            <div class="lg:text-9xl xs:text-5xl sm:text-5xl Gilroy-Bold text-cyan-500"> Work in Progress</div>
+            <div className="lg:text-9xl xs:text-5xl sm:text-5xl Gilroy-Bold text-cyan-500"> Work in Progress</div>
             <div className="text-2xl Gilroy-Light text-cyan-500"> - Siddharth Singh - </div>
         </div>
     );
