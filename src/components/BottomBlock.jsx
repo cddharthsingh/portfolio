36d99740a79a07ac117dc369fe2e096a5a0b0c36
Siddharth Singh <siddharth@xola.com>
2024-02-20 15:07:06 +0530

    Adding line column

--- src/components/BottomBlock.jsx
+++ src/components/BottomBlock.jsx
@@ -1,3 +1,7 @@
 export default function BottomBlock() {
-    return <div className="h-1/5 border-t fixed bottom-0 p-5 border-zinc-800 w-full text-cyan-700">Bottom Block</div>
+    return (
+        <div className="h-1/5 bg-zinc-950 border-t p-5 border-zinc-800 w-full text-cyan-700 fixed bottom-0">
+            Terminal
+        </div>
+    );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,3 @@
 export default function Sidebar() {
-    return <div className="w-1/6 border-r border-zinc-800 p-5 max-h-screen text-cyan-700">Sidebar</div>
+    return <div className="w-1/6 bg-zinc-950 border-r border-zinc-800 p-5 h-screen text-cyan-700">Explorer</div>;
 }
